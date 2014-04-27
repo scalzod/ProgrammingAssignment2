4a08b0afed69950d3174b8c1abeb91edbabd111c
scalzod <scalzod@gmail.com>
2014-04-27 12:45:59 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+## Learning to cache a potentially time consuming function and then perform a function using the cache.
+## e.g. cache the inverse of a matrix inversion for future operations.
 
 ## Write a short comment describing this function
 
