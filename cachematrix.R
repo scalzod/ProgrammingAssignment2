ba9a29eebe8538e4fab1170a14fb669b4f1feea4
scalzod <scalzod@gmail.com>
2014-04-27 14:05:25 -0700

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -2,7 +2,7 @@
 ## functions do:
 
 #### Learning to cache a potentially time consuming function and then perform a function using the cache.
-#### e.g. cache the inverse of a matrix inversion for future operations.
+#### e.g. cache a matrix inversion for future operations.
 
 ## Write a short comment describing this function:
 ##makeCacheMatrix <- function(x = matrix()) {
